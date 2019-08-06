3f1c20a420c064842d59d2487e0d033c9496541b
Sharon Watkins <swatkins876@gmail.com>
2019-08-06 14:05:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,8 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  pets = {"cat" => "Maru", "dog" => "Pluto"}
+  
 end
 
 def update_counting_hash(hash, key)
